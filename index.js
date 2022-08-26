eac5ddba7bf4ac082c9cb010133e22bc7608384b
+ <+>
2022-08-26 11:12:59 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 // Your code here
+
+// get Dodger
+const dodger = document.getElementById('dodger');
+dodger.style.backgroundColor ="#FF69B4";
+dodger.style.bottom ='100px';
+
+
+function moveDodgerLeft(){
+    const leftNumbers = dodger.style.left.replace("px", ""); 
+    const left = parseInt(leftNumbers, 10)
+
+    if(left > 0){
+        dodger.style.left =`${left-1}px`;
+    }
+
+    //creating an event listener
+    document.addEventListener("keydown", (e)=>{
+        if (e.key=="ArrowLeft"){
+            moveDodgerLeft();
+        }
+    })
+}
+
+function moveDodgerRight(){
+    const leftNumbers = dodger.style.left.replace("px", ""); 
+    const left = parseInt(leftNumbers, 10)
+
+    if(left > 0){
+        dodger.style.left =`${left+1}px`;
+    }
+
+    //creating an event listener
+    document.addEventListener("keydown", (e)=>{
+        if (e.key=="ArrowLeft"){
+            moveDodgerLeft();
+        }
+    })
+}
